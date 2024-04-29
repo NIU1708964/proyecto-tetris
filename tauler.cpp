8506e7411a22535a370c6bab773b408fd13df68f
yichen <1708964@uab.cat>
2024-04-29 18:20:36 +0200

    Actualización Tauler.h y Tauler.cpp

--- tauler.cpp
+++ tauler.cpp
@@ -1,6 +1,40 @@
 #include "tauler.h"
+#include "figura.h"
+#include <iostream>
+
+using namespace std;
 
 Tauler::Tauler()
 {
-
+    for (int i = 0; i < MAX_FILA; i++)
+    {
+        m_tauler[i][0] = NO_COLOR;
+        m_tauler[i][MAX_COL + 1] = NO_COLOR;
+        for (int j = 0; j < MAX_COL; j++)
+            m_tauler[i][j + 1] = COLOR_NEGRE;
+    }
+    for (int j = 0; j < MAX_COL + 4; j++)
+    {
+        m_tauler[MAX_FILA][j] = NO_COLOR;
+    }
+    for (int i = 0; i < MAX_FILA; i++)
+        m_lliures[i] = MAX_COL;
 }
+
+void Tauler::inicialitza(ColorFigura tauler[MAX_FILA][MAX_COL])
+{
+    for (int i = 0; i < MAX_FILA; i++)
+        for (int j = 0; j < MAX_COL; j++)
+        {
+            m_tauler[i][j + 1] = tauler[i][j];
+            if (tauler[i][j] != COLOR_NEGRE)
+                m_lliures[i]--;
+        }
+}
+
+bool Tauler::colisionaFigura(const Figura& figura)
+{
+    bool colisiona = false;
+    while()
+    return colisiona;
+}//son necesarios algunos metodos dentro de la clase figura
--- tauler.h
+++ tauler.h
@@ -1,18 +1,23 @@
-#include <iostream>
+#ifndef TAULER_H
+#define TAULER_H
+#include "Figura.h"
 
 #define MAX_COLUMNES = 8
 #define MAX_FILES = 8
+
 using namespace std;
 
 class Tauler()
 {
 private:
-	ColorFigura m_tauler[N_FILES][N_COLUMNES];
+
+	ColorFigura m_tauler[N_FILES + 1][N_COLUMNES + 2];// +1 a fila y +2 a columna para los bordes
 	int m_lliures[MAX_FILES]
+
 public:
 	Tauler();
-	void inicialitzaTauler();
-	bool colisionaFigura(); //utilitza metode colisiona dins de la classe figura
-	int getLliures(int fila);
-	//faltan metodes
+	void inicialitzaTauler(ColorFigura tauler[MAX_FILES][MAX_COLUMNES]); 
+	bool colisionaFigura(const Figura& figura);
+	int colocaFigura()
 };
+#endif
